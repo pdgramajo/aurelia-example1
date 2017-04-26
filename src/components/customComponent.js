c10da0fe544b1ac32c72e03457ba3ea5e4864e07
Pablo Gramajo <pgramajo@jbknowledge.com>
2017-04-26 16:06:14 -0300

    test2

--- src/components/customComponent.js
+++ src/components/customComponent.js
@@ -2,6 +2,9 @@ import {bindable} from "aurelia-framework";
 
 export  class CustomComponent {
     @bindable data;
+    /**
+     * conntructor without parameters
+     */
     constructor(){
      
     }
